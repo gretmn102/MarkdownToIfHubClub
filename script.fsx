840c40e9e535da72e682e9310ad54d30d9849677
gretmn102 <treesortbubble@gmail.com>
2026-02-20 22:51:28 +0200

    refactor: move library code to `src/Library.fsx`

--- script.fsx
+++ script.fsx
@@ -1,51 +1,6 @@
 #!/usr/bin/env -S dotnet fsi
-#r "nuget: Markdig, 0.45.0"
-open Markdig
-
-let shiftHeaderLevel (markdownDocument: Syntax.MarkdownDocument) =
-    let rec mapBlocks (blocks: Syntax.Block seq) =
-        blocks
-        |> Seq.iter (function
-            | :? Syntax.HeadingBlock as headingBlock ->
-                headingBlock.Level <- headingBlock.Level + 3
-            | _ -> ()
-        )
-    mapBlocks markdownDocument
-
-let private markdownPipeline =
-    let pipe = MarkdownPipelineBuilder()
-
-    pipe.UseSoftlineBreakAsHardlineBreak() |> ignore
-
-    let opt = Extensions.AutoLinks.AutoLinkOptions()
-    opt.OpenInNewWindow <- true
-    opt.UseHttpsForWWWLinks <- true
-    pipe.UseAutoLinks opt |> ignore
-
-    pipe.UseCitations() |> ignore
-    pipe.UseFigures() |> ignore
-    pipe.UseFooters() |> ignore
-
-    pipe.UseFootnotes() |> ignore
-
-    pipe.UseAutoIdentifiers Extensions.AutoIdentifiers.AutoIdentifierOptions.AutoLink |> ignore
-
-    pipe.UseMediaLinks() |> ignore
-
-    pipe.UsePipeTables() |> ignore
-
-    pipe.UseGenericAttributes() |> ignore
-
-    pipe.UseEmphasisExtras() |> ignore // for ~~strike~~
-
-    pipe.Build()
-
-let convert rawMarkdown =
-    let document = Markdig.Markdown.Parse(rawMarkdown, markdownPipeline)
-    shiftHeaderLevel document
-    use writer = new System.IO.StringWriter()
-    let render = Renderers.HtmlRenderer writer
-    render.Render(document).ToString()
+#load "src/Library.fsx"
+open Library
 
 #if !INTERACTIVE
 [<EntryPoint>]
